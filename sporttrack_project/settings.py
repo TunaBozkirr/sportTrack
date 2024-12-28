f9302ad0d09a18485dfd069311f7b0e6d1b1ff96
Tuna Bozkir <34109169+TunaBozkirr@users.noreply.github.com>
2024-12-28 14:17:53 +0100

    Update settings.py

--- sporttrack_project/settings.py
+++ sporttrack_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(qg!0f!6aa^#rby_uu)swoh$pivk8anvu4g004*7jwo#y5)x9('
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
